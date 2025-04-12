027864703f112e6d07bed50db7d054d9c216d4d4
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-04-12 18:02:21 -0300

    RoadMapJS

--- bucles/nivel2/nivel2.js
+++ bucles/nivel2/nivel2.js
@@ -2,7 +2,7 @@
 for(let fila = 1; fila<=3; fila++){
     let contenido = ""
     for(let columna = 1; columna <= 4; columna++){
-        contenido += "#"
+        contenido += "*"
     }
     console.log(contenido)
 }
@@ -61,6 +61,113 @@ for(let f = 1; f <= 4; f++){
     console.log(contenido)
 }
 */
+
+/*Ejercicio 7
+for(let f= 1; f <= 5; f++){
+    let cont = ""
+    for(let c = 1; c <= 5; c++){
+        if((f + c) % 2 === 0){
+            cont += c
+        }else{
+            cont += "*"
+        }
+        if(c < 5){
+            cont += " "
+        }
+    }
+    console.log(cont)
+}
+*/
+
+/* Ejercicio 8
+for(let f = 1; f <= 5; f++){
+    let cont = "";
+    for(let c = 1; c <= 5; c++){
+        if( (f+c) % 2 === 0 ){
+            cont += f * c
+        }else{
+            cont += "-"
+        }
+        if(c < 5){
+            cont += " "
+        }
+    }
+    console.log(cont)
+}
+*/
+/* Ejercicio 9
+for(let f = 1; f <= 5; f++){
+    let cont =""
+    for(let c = 1; c <= 5; c++){
+        if( f === c ){
+            cont += "1"
+        }else{
+            cont += "0"
+        }
+        if( c < 5){
+            cont += " "
+        }
+    }
+    console.log(cont.trim())
+}
+*/
+
+/* Ejercicio 10
+for( let f = 1; f <= 5; f++){
+    let cont = "";
+    for(let c = 1; c <= 5; c++){
+        if( (f + c) === 6 ){
+            cont += "1";
+        } else {
+            cont += "0";
+        }
+        if(c < 5){
+            cont += " ";
+        }
+    }
+    console.log(cont)
+}
+*/
+
+/* Ejercicio 11
+for( let f = 1; f <= 5; f++){
+    let cont = "";
+    for(let c = 1; c <= 5; c++){
+        if(f === c || (f+c) === 6){
+            cont += "1";
+        }else{
+            cont += "0";
+        }
+        if(c < 5){
+            cont += " ";
+        }
+    }
+    console.log(cont)
+}
+*/
+
+/*
+for( let f = 1; f <= 7; f++){
+    let cont = "";
+    for( let c = 1; c <= 7; c++){
+        if(f === 4 && c === 4){
+            cont += "X";
+        } else if(f === c || (f + c) === 8){
+            cont += "/";
+        } else{
+            cont += ".";
+        }
+        if(c < 8){
+            cont += " "
+        }
+    }
+    console.log(cont)
+}
+*/
+
+
+
+
 
 /*
             + + + + + + +
